d753bfcb0b0da4a12c077421dbe2934e3eea63c4
12112002nmtTOAN <NGManhToan>
2023-05-10 22:40:25 +0700

    Add profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/home_page.dart';
+import 'package:flutter_application_2/profile_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,13 +32,17 @@ class RootPage extends StatefulWidget {
 
 class _RootPageState extends State<RootPage> {
   int currentPage = 0;
+  List<Widget> pages = const [
+    HomePage(),
+    ProfilePage(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter'),
       ),
-      body: const HomePage(),
+      body: pages[currentPage],
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           debugPrint('floatingActionButton');
